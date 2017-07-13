e8f50b39108bbbe4ea3454fb2e33ad2a0fddc438
Hui Wang <huiw3939@gmail.com>
2017-07-12 23:39:07 -0400

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,41 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_hash[:winter].each do |x,y|
+y.push(supply)
+end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+holiday_hash[:spring][:memorial_day].push(supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+holiday_hash[season][holiday_name] = supply_array
+holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+arr = []
+holiday_hash[:winter].each do |holi,val|
+  val.each do |i|
+    arr.push(i)
+  end
+end
+arr
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +63,37 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |seas,holi|
+  puts "#{seas.capitalize}:"
+  holi.each do |holi, sup|
+    str = ""
+    sup.each do |x|
+      if sup[-1] == x
+      str += x
+    else
+      str += x+", "
+    end
+    end
+    holistr = holi.to_s
+    if holistr.include?("_")
+      holistr = holistr.split("_").collect{|x| x.capitalize!}.join(" ")
+    else holistr = holistr.capitalize
+    end
+    puts "  #{holistr}: #{str}"
+  end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  arr = []
+holiday_hash.collect do |sea,hol|
+  hol.collect do |x,y|
+  if y.include?("BBQ")
+    arr.push x
+  end
 end
-
-
-
-
-
-
-
+end
+arr
+end
